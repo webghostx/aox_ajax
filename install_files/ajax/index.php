0e8f424bd6e5ebd9979b9ab6c1595b610ca574cf
webghostx <webghost779@gmail.com>
2015-06-13 17:05:52 +0200

    --

---
+++ install_files/ajax/index.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ *  wenn der Aufruf nicht per ajax gemacht wird, sofort beenden
+ */
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest') {
+      header('HTTP/1.1 400 Bad Request');
+      exit();
+}
+
+/*
+ * REDAXO Kern und Addons laden
+ */
+unset($REX);
+$REX['REDAXO'] = false;
+$REX['GG'] = true;
+$REX['HTDOCS_PATH'] = '../';
+require_once realpath($REX['HTDOCS_PATH'] . 'redaxo/include/functions/function_rex_mquotes.inc.php');
+require_once realpath($REX['HTDOCS_PATH'] . 'redaxo/include/master.inc.php');
+require_once realpath($REX['HTDOCS_PATH'] . 'redaxo/include/addons.inc.php');
+require_once $REX['ADDON']['aox_ajax']['settings']['CONTROLLER_CLASS'];
+
+/*
+ * Debug Status aus Request entnehmen
+ */
+if(isset($_REQUEST['DEBUG']) && $_REQUEST['DEBUG'] !== false)
+      $REX['ADDON']['aox_ajax']['settings']['DEBUG'] = true;
+
+/*
+ * Controller starten
+ */
+$ajax = aoxAjaxController::run($REX, $_REQUEST, $REX['ADDON']['aox_ajax']['settings']['DEBUG']);
---
+++ install_files/ajax/note.php
@@ -0,0 +1,9 @@
+<?php
+exit();
+/**
+ * Dieses Verzeichnis ist Teil des Ajax Frameworks (aox_ajax) für REDAXO
+ * 
+ * Im Ordner PHP können Scripte abgelegt werden welche auf einen Ajax-Request
+ * antworten sollen. Ein direkter Aufruf über eine URL wird verhindert.
+ * 
+ */
